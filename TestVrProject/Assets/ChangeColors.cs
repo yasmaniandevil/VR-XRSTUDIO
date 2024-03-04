0c5eb1928a84a83d7982eff1f299f817b064565b
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2024-03-04 18:56:43 -0500

    added script and objs

--- TestVrProject/Assets/ChangeColors.cs
+++ TestVrProject/Assets/ChangeColors.cs
@@ -6,10 +6,34 @@ using Meta;
 
 public class ChangeColors : MonoBehaviour
 {
+
+    public Material[] materials;
+    private int currentIndex = 0;
+    private MeshRenderer mr;
+
     // Start is called before the first frame update
     void Start()
     {
+        mr = GetComponent<MeshRenderer>();
         
+        if(materials.Length > 0)
+        {
+            mr.material = materials[currentIndex];
+        }
+
+       
+    }
+
+    public void ChangeColor()
+    {
+        if(materials.Length == 0)
+        {
+            Debug.Log("No Materials assigned");
+            return;
+        }
+
+        currentIndex = (currentIndex + 1) % materials.Length;
+        mr.material = materials[currentIndex];
     }
 
     // Update is called once per frame
